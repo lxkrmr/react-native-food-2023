d75ab3ff1cdbbe51b442a3afd522a850889c52c1
Alex <lxkrmr@icloud.com>
2023-03-08 15:18:13 +0100

    💩 add simple error handling for yelp api

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -6,17 +6,24 @@ import yelp from '../api/yelp';
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const searchApi = async () => {
-    const response = await yelp.get('/search', {
-      params: { term, limit: 50, location: 'san jose' },
-    });
-    setResults(response.data.businesses);
+    try {
+      const response = await yelp.get('/search', {
+        params: { term, limit: 50, location: 'san jose' },
+      });
+      setResults(response.data.businesses);
+      setErrorMessage('');
+    } catch (err) {
+      setErrorMessage('Something went wrong');
+    }
   };
 
   return (
     <View>
       <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
+      {errorMessage && <Text>{errorMessage}</Text>}
       <Text>We have found {results.length}</Text>
     </View>
   );
