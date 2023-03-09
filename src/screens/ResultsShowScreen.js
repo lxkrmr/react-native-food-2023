a74cdd497428f667c1f0377cc04dbe4b6702a963
Alexander Kramer <lxkrmr@icloud.com>
2023-03-09 19:25:19 +0100

    ✨ finish ResultsShowScreen for now

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,14 +1,43 @@
-import { Text, StyleSheet } from "react-native";
+import { useState, useEffect } from "react";
+import { Text, FlatList, Image, StyleSheet } from "react-native";
+import yelp from "../api/yelp";
 
 const ResultsShowScreen = ({ navigation }) => {
+  const [result, setResult] = useState(null);
   const id = navigation.getParam("id");
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return result;
+  }
+
   return (
     <>
-      <Text>Results Show Screen {id}</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image source={{ uri: item }} style={styles.imageStyle} />;
+        }}
+      />
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  imageStyle: {
+    height: 200,
+    width: 300,
+    borderRadius: 4,
+  },
+});
 
 export default ResultsShowScreen;
